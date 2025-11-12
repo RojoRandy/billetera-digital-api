13c928cd6480ecbdbbbeadd163f3591f459ef36c
RandyRojo <desarrollo.mytcr@gmail.com>
2025-11-11 21:52:04 -0600

    Estandarización de respuesta a usuario

--- src/shared/infrastructure/dto/response.dto.ts
+++ src/shared/infrastructure/dto/response.dto.ts
@@ -2,19 +2,11 @@
 export class ResponseDto<T> {
   private constructor(
     public data: T | T[],
-    public message: string
+    public message: string,
+    public success: boolean = true
   ) {}
 
-  public static create<T>({
-    data,
-    message
-  }: {
-    data: T | T[],
-    message: string
-  }): ResponseDto<T> {
-    return new this(
-      data,
-      message
-    )
+  public static create<T>(data: T | T[], message: string = 'Success'): ResponseDto<T> {
+    return new this(data,message);
   }
 }
--- src/shared/infrastructure/interceptor/response.interceptor.ts
+++ src/shared/infrastructure/interceptor/response.interceptor.ts
@@ -2,14 +2,13 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-export interface Response<T> {
-  data: T;
-}
+import { ResponseDto } from '../dto/response.dto';
 
 @Injectable()
-export class ApiResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
-    return next.handle().pipe(map(data => ({ data })));
+export class ApiResponseInterceptor<T> implements NestInterceptor<T, ResponseDto<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<ResponseDto<T>> {
+    return next.handle().pipe(map(data => {
+      return ResponseDto.create<T>(data)
+    }));
   }
 }
